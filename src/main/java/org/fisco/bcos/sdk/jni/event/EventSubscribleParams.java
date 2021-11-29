6f10d584ecfa2bf18433796d13c979115418863e
octopus <912554887@qq.com>
2021-11-30 06:35:06 +0800

    add eventsub demo

--- src/main/java/org/fisco/bcos/sdk/jni/event/EventSubscribleParams.java
+++ src/main/java/org/fisco/bcos/sdk/jni/event/EventSubscribleParams.java
@@ -1,42 +1,43 @@
 package org.fisco.bcos.sdk.jni.event;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class EventSubscribleParams {
-    private long fromBlock;
-    private long toBlock;
-    private List<String> address;
-    private List<List<String>> topics;
+  private long fromBlock;
+  private long toBlock;
+  private List<String> addresses = new ArrayList<>();
+  private List<List<String>> topics = new ArrayList<>();
 
-    public long getFromBlock() {
-        return fromBlock;
-    }
+  public long getFromBlock() {
+    return fromBlock;
+  }
 
-    public void setFromBlock(long fromBlock) {
-        this.fromBlock = fromBlock;
-    }
+  public void setFromBlock(long fromBlock) {
+    this.fromBlock = fromBlock;
+  }
 
-    public long getToBlock() {
-        return toBlock;
-    }
+  public long getToBlock() {
+    return toBlock;
+  }
 
-    public void setToBlock(long toBlock) {
-        this.toBlock = toBlock;
-    }
+  public void setToBlock(long toBlock) {
+    this.toBlock = toBlock;
+  }
 
-    public List<String> getAddress() {
-        return address;
-    }
+  public List<String> getAddresses() {
+    return addresses;
+  }
 
-    public void setAddress(List<String> address) {
-        this.address = address;
-    }
+  public void setAddresses(List<String> addresses) {
+    this.addresses = addresses;
+  }
 
-    public List<List<String>> getTopics() {
-        return topics;
-    }
+  public List<List<String>> getTopics() {
+    return topics;
+  }
 
-    public void setTopics(List<List<String>> topics) {
-        this.topics = topics;
-    }
+  public void setTopics(List<List<String>> topics) {
+    this.topics = topics;
+  }
 }
---
+++ src/main/java/org/fisco/bcos/sdk/jni/test/eventsub/EventSub.java
@@ -0,0 +1,77 @@
+package org.fisco.bcos.sdk.jni.test.eventsub;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Arrays;
+import org.fisco.bcos.sdk.jni.common.JniConfig;
+import org.fisco.bcos.sdk.jni.common.JniException;
+import org.fisco.bcos.sdk.jni.common.Response;
+import org.fisco.bcos.sdk.jni.event.EventSubscribe;
+import org.fisco.bcos.sdk.jni.event.EventSubscribeCallback;
+import org.fisco.bcos.sdk.jni.event.EventSubscribleParams;
+import org.fisco.bcos.sdk.jni.test.Utility;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EventSub {
+
+  private static final Logger logger = LoggerFactory.getLogger(EventSub.class);
+
+  public static void usage() {
+    System.out.println("\tUsage: ");
+    System.out.println(
+        "\t\tjava -cp \"conf/:lib/*:apps/*\"  org.fisco.bcos.sdk.jni.test.event.EventSub ip:port group fromBlk toBlk");
+    System.out.println("\tExample:");
+    System.out.println(
+        "\t\tjava -cp \"conf/:lib/*:apps/*\"  org.fisco.bcos.sdk.jni.test.event.EventSub 127.0.0.1:20201 group -1 -1");
+    System.exit(0);
+  }
+
+  public static void main(String[] args)
+      throws InterruptedException, JniException, JsonProcessingException {
+    if (args.length < 4) {
+      usage();
+    }
+
+    String peer = args[0];
+    String group = args[1];
+    long fromBlk = Long.valueOf(args[2]);
+    long toBlk = Long.valueOf(args[3]);
+
+    System.out.println("EventSub sample: ");
+    System.out.println("\t group: " + group);
+    System.out.println("\t fromBlk: " + fromBlk);
+    System.out.println("\t toBlk: " + toBlk);
+
+    JniConfig jniConfig = Utility.newJniConfig(Arrays.asList(peer));
+    jniConfig.setDisableSsl(true);
+    EventSubscribe eventSubscribe = EventSubscribe.build(jniConfig);
+
+    EventSubscribleParams params = new EventSubscribleParams();
+    params.setFromBlock(fromBlk);
+    params.setToBlock(toBlk);
+
+    ObjectMapper objectMapper = new ObjectMapper();
+    String strParams = objectMapper.writeValueAsString(params);
+
+    System.out.println("\t params: " + strParams);
+
+    eventSubscribe.start();
+    eventSubscribe.subscribeEvent(
+        group,
+        objectMapper.writeValueAsString(params),
+        new EventSubscribeCallback() {
+          @Override
+          public void onReceiveLog(Response response) {
+            System.out.println("subscribeEvent ==>>> " + response.getErrorCode());
+            System.out.println("\t errorCode: " + response.getErrorCode());
+            System.out.println("\t errorMessage: " + response.getErrorMessage());
+            System.out.println("\t data: " + response.getData());
+          }
+        });
+
+    while (true) {
+      Thread.sleep(10000);
+    }
+  }
+}

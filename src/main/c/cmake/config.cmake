26e4019ae371614ee770c30e2aa1259e72b2f000
ywy2090 <912554887@qq.com>
2022-02-10 11:23:47 +0800

    update bcos-boostssl (#74) (#75)

--- src/main/c/cmake/config.cmake
+++ src/main/c/cmake/config.cmake
@@ -24,18 +24,18 @@ hunter_config(bcos-crypto VERSION 3.0.0-rc3-local
 
 hunter_config(bcos-boostssl
 	VERSION 3.0.0-local
-	URL https://${URL_BASE}/FISCO-BCOS/bcos-boostssl/archive/685288e31c45ab983fc68b789ba81838948e2991.tar.gz
-	SHA1 ba076ca3770b5a59e58dcc1772c99718d29f3854
+	URL https://${URL_BASE}/FISCO-BCOS/bcos-boostssl/archive/16ce8a9f1125b86b18e888bcc1267d0ec6bf2d38.tar.gz
+	SHA1 e887ef537a387dcb122f5fc30ac91b5889dac412
 )
 
 hunter_config(bcos-cpp-sdk
 	VERSION 3.0.0-local
-	URL https://${URL_BASE}/FISCO-BCOS/bcos-cpp-sdk/archive/f81727f912280d3d6814fde44c21116fc04982d4.tar.gz
-	SHA1 96c003fec575fe4f6b66997e99b9dd9c34a24c62
+	URL https://${URL_BASE}/FISCO-BCOS/bcos-cpp-sdk/archive/bd8a0059d742bfdc112a1b0ef41db158903db71c.tar.gz
+	SHA1 412fc661af95ad37d4b5f6627b483595effa76e1
 )
 
 hunter_config(bcos-c-sdk
 	VERSION 3.0.0-local
-	URL https://${URL_BASE}/FISCO-BCOS/bcos-c-sdk/archive/4e9ea73bc09aebcd20edf5c103dec0bc1385686c.tar.gz
-	SHA1 5136bdfc33c645dda9490211bffce5c2395b7a05
+	URL https://${URL_BASE}/FISCO-BCOS/bcos-c-sdk/archive/1735a5faf2927c3baa934c5c09fc5022ce669ff1.tar.gz
+	SHA1 1f43b6841ee21a10b4bdfb2e2b3968289f9caf6a
 )
